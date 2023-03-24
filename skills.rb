760b42022a857087c7ad6adc1bb2bb4281a4f2e0
Chazz Gagliano <cgags17285@gmail.com>
2023-03-24 00:43:01 -0400

    message here

--- skills.rb
+++ skills.rb
@@ -7,8 +7,8 @@
 
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 
-array = ["a", "b", "c", "d"]
-array[1] = 34
+# array = ["a", "b", "c", "d"]
+# array[1] = 34
 
 # p array
 
