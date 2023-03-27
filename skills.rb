50f1835d94ab598c116991f02758b73fb69ed55b
Chazz Gagliano <cgags17285@gmail.com>
2023-03-26 21:32:16 -0400

    function questions 1-10 complete

--- skills.rb
+++ skills.rb
@@ -56,15 +56,27 @@
 
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
-def string_five(string)
-  return string * 5 
-end
+# def string_five(string)
+#   return string * 5 
+# end
 
-p string_five("pig")
+# p string_five("pig")
 
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
+def average(number1, number2, number3)
+  return (number1 + number2 + number3) / 3
+end
+
+# p average(2, 3, 4)
+
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+def ten_thirty(number)
+  return (number * 10) + 30
+end
+
+p ten_thirty(4)
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/6a26d3c698c651dd6e9ccb168d32648c
