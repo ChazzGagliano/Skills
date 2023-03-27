f51f59af7de7baf8467b258dcd3c62495fcf7dce
Chazz Gagliano <cgags17285@gmail.com>
2023-03-26 21:52:24 -0400

    questions 3-4 complete

--- skills.rb
+++ skills.rb
@@ -1,29 +1,44 @@
 #  1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
 
-numbers = [1, 2, 3]
+# numbers = [1, 2, 3]
 
-new_numbers = numbers.map do |number|
- number * 3 
-end
+# new_numbers = numbers.map do |number|
+#  number * 3 
+# end
 
-p new_numbers
+# p new_numbers
 
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
-array = ["hello", "goodbye"]
-new_array = array.map do |strings|
-  strings.upcase
-end
+# array = ["hello", "goodbye"]
+# new_array = array.map do |strings|
+#   strings.upcase
+# end
 
-p new_array
+# p new_array
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+hashes = [{name: "john", age: "22"}, {name: "tim", age: "25"}]
+
+only_names = hashes.map do |hash|
+  hash[:name]
+end
+
+# p only_names
+
 
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
+
+numbers = [1, 2, 3]
+new_numbers = numbers.map do |number|
+  number + 7
+end
+
+p new_numbers
 
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
