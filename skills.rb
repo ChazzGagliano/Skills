83a86aa61c3781dfd5461955b285b68bd77a53db
Chazz Gagliano <cgags17285@gmail.com>
2023-03-28 00:24:58 -0400

    9/10 complete

--- skills.rb
+++ skills.rb
@@ -80,7 +80,7 @@ new_strings = strings.select do |string|
   string[0] != "b"
 end
 
-p new_strings
+# p new_strings
 
 
 
@@ -88,9 +88,22 @@ p new_strings
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+cheap_items = items.select do |item|
+  item[:price] < 10
+end
+
+# p cheap_items
+
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
 
+numbers = [2, 4, 5, 1, 8, 9, 7]
+odd_numbers = numbers.select do |number|
+  number % 2 == 1 
+end
+
+p odd_numbers
 
 # SOLUTIONS (using while loop): https://gist.github.com/peterxjang/7de16ed43ea506e98df3fa15074b84f8
 # SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/a702894841c7018ed8c127b647ae21f8
