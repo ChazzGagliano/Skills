cc0354c214501ab815a9fbd8504b254d018ebc7b
Chazz Gagliano <cgags17285@gmail.com>
2023-03-29 00:51:22 -0400

    1/2 complete

--- skills.rb
+++ skills.rb
@@ -1,8 +1,23 @@
 #  1. Start with an array of numbers and compute the sum of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 26.
+array = [5, 10, 8, 3]
+sum = 0
+array.each do |number|
+  sum += number
+end
+
+p sum
 
 #  2. Start with an array of strings and combine them all into a single string.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+array = ["volleyball", "basketball", "badminton"] 
+sentence = ""
+array.each do |word|
+  sentence += word
+end
+
+p sentence
 
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
