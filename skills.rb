749134fc940fd5873f686162ded27040e2a040e2
Chazz Gagliano <cgags17285@gmail.com>
2023-03-29 00:58:51 -0400

    3/4 complete

--- skills.rb
+++ skills.rb
@@ -6,7 +6,7 @@ array.each do |number|
   sum += number
 end
 
-p sum
+# p sum
 
 #  2. Start with an array of strings and combine them all into a single string.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
@@ -17,13 +17,32 @@ array.each do |word|
   sentence += word
 end
 
-p sentence
+# p sentence
 
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
 
+array = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+
+sum = 0
+
+array.each do |number|
+  sum += number[:price]
+end
+
+p sum
+
 #  4. Start with an array of numbers and compute the the minumum number.
 #     For example, [5, 10, 8, 3, 9] becomes 3.
+array = [5, 10, 8, 3, 9]
+min = array[0]
+array.each do |number|
+  if number < min
+  min = number
+  end
+end
+
+p min
 
 #  5. Start with an array of strings and compute the total length of all the strings.
 #     For example, ["volleyball", "basketball", "badminton"] becomes 29.
